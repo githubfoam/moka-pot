d88263106d6528d6ec632a9de69a178236f7f53d
githubfoam <lequestionis@gmail.com>
2020-06-30 01:08:58 +0300

    крупным

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -9,7 +9,7 @@ set -o xtrace
   #Ansible provides its official PPA for the installation on Debian systems
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  apt update -qq && apt-get install ansible --qqy
+  apt-get update -qq && apt-get install --qq ansible
   ansible --version
 
   echo "===================================================================================="
