c0cd60ad0cd1ed30a1500bbe346a2ec8c0846e51
githubfoam <lequestionis@gmail.com>
2020-06-29 19:59:20 +0300

    чин`овником

--- scripts/deploy-boxes.sh
+++ scripts/deploy-boxes.sh
@@ -14,12 +14,18 @@ vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with differ
 # # vagrant up --provider=libvirt
 # vagrant up --provider=libvirt vg-mokapot-01
 
+
+
+
 # adapter to enable NFS on this machine for Vagrant.
 # Please verify that `nfsd` is installed on your machine, and try again
 # HOST is not windows.
 # HOST is travisci bionic server
 # vagrant cloud debian box specific
 apt-get install nfs-common nfs-kernel-server -qqy
+visudo -f /etc/sudoers.d/vagrant-nfs
+
+# https://www.debian.org/releases/
 
 #https://app.vagrantup.com/debian/boxes/buster64
 vagrant box add "debian/buster64" --provider=libvirt
@@ -28,6 +34,7 @@ vagrant up --provider=libvirt "vg-mokapot-02"
 #https://app.vagrantup.com/debian/boxes/stretch64
 vagrant box add "debian/stretch64" --provider=libvirt
 vagrant up --provider=libvirt "vg-mokapot-03"
+
 
 #https://github.com/chef/bento/tree/master/packer_templates/debian
 vagrant box add "bento/debian-9.12" --provider=virtualbox
@@ -38,6 +45,7 @@ vagrant up --provider=libvirt "vg-mokapot-04"
 vagrant box add "bento/debian-10.4" --provider=virtualbox
 vagrant mutate "bento/debian-10.4" libvirt
 vagrant up --provider=libvirt "vg-mokapot-05"
+
 
 vagrant box list #veridy installed boxes
 vagrant status #Check the status of the VMs to see that none of them have been created yet
