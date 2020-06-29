72f650aa5fbbaeb58031426df26019c5b3d9d444
githubfoam <lequestionis@gmail.com>
2020-06-29 18:36:03 +0300

    чин`овника

--- scripts/deploy-boxes.sh
+++ scripts/deploy-boxes.sh
@@ -9,10 +9,16 @@ echo "==========================================================================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-vagrant box add "bento/ubuntu-19.10" --provider=virtualbox
-vagrant mutate "bento/ubuntu-19.10" libvirt
+# vagrant box add "bento/ubuntu-19.10" --provider=virtualbox
+# vagrant mutate "bento/ubuntu-19.10" libvirt
+# # vagrant up --provider=libvirt
+# vagrant up --provider=libvirt vg-mokapot-01
+
+vagrant box add "bento/debian-9.12" --provider=virtualbox
+vagrant mutate "bento/debian-9.12" libvirt
 # vagrant up --provider=libvirt
-vagrant up --provider=libvirt vg-mokapot-01
+vagrant up --provider=libvirt "vg-foremansrv04"
+
 
 vagrant box list #veridy installed boxes
 vagrant status #Check the status of the VMs to see that none of them have been created yet
