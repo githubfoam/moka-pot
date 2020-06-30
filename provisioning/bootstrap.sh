65c49174d08a6f30680d48286bb167ab036c3c5f
githubfoam <lequestionis@gmail.com>
2020-06-30 03:06:48 +0300

    фарцовщик

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -5,14 +5,6 @@ set -o nounset
 set -o xtrace
 # set -eox pipefail #safety for script
 
-  ######vg-mokapot-05: /tmp/vagrant-shell: line 16: ansible: command not found ###
-  #Ansible provides its official PPA for the installation on Debian systems
-  # echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
-  # apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  # apt update
-  # apt install ansible -y
-  # ansible --version
-
   echo "===================================================================================="
                         hostnamectl status
   echo "===================================================================================="
