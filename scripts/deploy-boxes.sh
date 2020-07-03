3392a9effd4619f9247c4986bc12a62843cc16d2
githubfoam <lequestionis@gmail.com>
2020-07-03 17:01:41 +0300

    с`отни

--- scripts/deploy-boxes.sh
+++ scripts/deploy-boxes.sh
@@ -31,7 +31,7 @@ vagrant up --provider=libvirt "vg-mokapot-02"
 # vagrant mutate "bento/debian-10.4" libvirt
 # vagrant up --provider=libvirt "vg-mokapot-05"
 
-https://app.vagrantup.com/archlinux
+# https://app.vagrantup.com/archlinux
 vagrant box add "archlinux/archlinux" --provider=libvirt
 vagrant up --provider=libvirt "vg-mokapot-06"
 
